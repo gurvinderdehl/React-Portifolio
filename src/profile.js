2b3922ef165d4114d71c682035f22afdd2a23b02
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-15 00:22:34 +0100

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -7,3 +7,42 @@ const animation = {
     
     once: true
 }
+
+const header = {
+    name: "Gurvinder",
+}
+const background = {
+    
+    type: "Snow"
+}
+
+const section2title = "About Me"
+const about = {
+    paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. Orci phasellus egestas tellus rutrum tellus pellentesque eu. Dapibus ultrices in iaculis nunc. Pretium nibh ipsum consequat nisl vel. Consequat ac felis donec et odio. Orci nulla pellentesque dignissim enim sit amet. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu."
+}
+
+const skillsBar = [
+    {
+        name: "HTML5",
+        faClass: "fab fa-html5"
+    },{
+        name: "CSS3",
+        faClass: "fab fa-css3"
+    },{
+        name: "Javascript",
+        faClass: "fab fa-js"
+    },{
+        name: "Node",
+        faClass: "fab fa-node"
+    },{
+        name: "Java",
+        faClass: "fab fa-java"
+    },{
+        name: "NPM",
+        faClass: "fab fa-npm"
+    },{
+        name: "Heroku",
+        faClass: "fas fa-h-square"
+    }
+      
+]
