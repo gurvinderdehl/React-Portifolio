dd60ad6304cc4f4a3019f80aca6de5aff8f214f3
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-15 00:13:05 +0100

    files changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
-import Portifolio from "./pages/Portifolio";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import Particles from './components/layouts/Particles'
+import Header from './components/pages/Header'
+import About from './components/pages/About'
+import Works from './components/pages/Works'
+import Contact from './components/pages/Contact'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { animation } from './profile'
 
-function App () {
- return (
-   <Router>
-     <Route exact path ="/" component={Portifolio} />
-   </Router>
- )
+
+function App() {
+
+  useEffect(() => {
+      AOS.init({
+        duration: animation.duration,
+        once: animation.once,
+        disable: !animation.animate
+      })
+// eslint-disable-next-line
+  }, [])
+
+  return (
+    <div className="App">
+      <Header />
+      <Particles />
+      <About />
+      <Works />
+      <Contact />
+    </div>
+  );
 }
 
-
-export default App; 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css'
+import './styles/images.css'
+import './styles/no-touch.min.css'
 
 ReactDOM.render(
-  
-    <App />,
-  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
